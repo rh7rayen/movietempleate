c8b42801c2fce1e36f0f499f9a5e0538f58d1e3c
rh7rayen <80848365+rh7rayen@users.noreply.github.com>
2023-04-09 04:39:54 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    center: true,
+    autoplay: true,
+    timeout: 3000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+});
